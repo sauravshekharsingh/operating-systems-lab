d45f43bf4e8f635a9f5af18d5500e729f85c58d2
Saurav Shekhar Singh <62594900+sauravshekharsingh@users.noreply.github.com>
2021-09-26 22:45:20 +0530

    Update 1 Fork.c

--- Lab2/1 Fork.c
+++ Lab2/1 Fork.c
@@ -5,7 +5,10 @@
 int main() {
     // Create child process
     int id = fork();
+    
     printf("Process %d\n", id);
+
+    // Wait for the child process to finish
     wait(NULL);
     return 0;
 }
