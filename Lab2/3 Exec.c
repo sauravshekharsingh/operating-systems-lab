c31f462b1d66481d89e278c154230e9ed47cce52
Saurav Shekhar Singh <62594900+sauravshekharsingh@users.noreply.github.com>
2021-09-26 22:56:10 +0530

    Update 3 Exec.c

--- Lab2/3 Exec.c
+++ Lab2/3 Exec.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     printf("In the main process\n");
     printf("PID of main process: %d\n",getpid());
+
     char *args[] = {"./example", "a", "b", NULL};
     execv(args[0], args);
+    
     printf("Back to main process\n");
     return 0;
 }
