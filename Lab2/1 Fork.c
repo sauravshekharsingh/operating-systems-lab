5befba0c3250677cdb1892547333407871f18d91
Saurav Shekhar Singh <62594900+sauravshekharsingh@users.noreply.github.com>
2021-09-26 22:44:49 +0530

    Update 1 Fork.c

--- Lab2/1 Fork.c
+++ Lab2/1 Fork.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/wait.h>
+
 int main() {
-    int val = fork();
-    printf("Process %d\n", val);
+    // Create child process
+    int id = fork();
+    printf("Process %d\n", id);
+    wait(NULL);
     return 0;
 }
