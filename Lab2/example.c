5bebb4ceceba2320741897ebebcd0170f5cb104e
Saurav Shekhar Singh <62594900+sauravshekharsingh@users.noreply.github.com>
2021-09-26 22:56:24 +0530

    Update example.c

--- Lab2/example.c
+++ Lab2/example.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     printf("In the exec process\n");
-    printf("PID of exec process: %d\n",getpid());
+    printf("PID of exec process: %d\n", getpid());
+    
+    // Print all the arguments
     puts(argv[1]);
     puts(argv[2]);
+
     return 0;
 }
