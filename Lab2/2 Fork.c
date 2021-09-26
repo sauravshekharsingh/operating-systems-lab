e9b561efbb0d54935a105f02fdb750346dbefdb0
Saurav Shekhar Singh <62594900+sauravshekharsingh@users.noreply.github.com>
2021-09-26 22:48:51 +0530

    Update 2 Fork.c

--- Lab2/2 Fork.c
+++ Lab2/2 Fork.c
@@ -4,15 +4,25 @@
 #include<sys/wait.h>
 
 int main() {
+    // Create child process
     int id = fork();
     int status;
+    
+    if(id == -1) {
+        printf("Error occured with fork\n");\
+        return 1;
+    }
+
     if(id == 0) {
         printf("Child process\n");
         printf("PID=%d, PPID=%d\n",getpid(),getppid());
+        
         exit(10);
     } else {
         printf("Parent process\n");
         printf("PID=%d, PPID=%d\n",getpid(),getppid());
+        
+        // Wait for child process
         wait(&status);
         if (WIFEXITED(status)) {
             printf("Exit status of child: %d\n", WEXITSTATUS(status));
