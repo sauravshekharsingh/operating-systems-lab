bbddb3abf77299df1913af441dcf1fd3f62f1178
Saurav Shekhar Singh <62594900+sauravshekharsingh@users.noreply.github.com>
2021-10-01 16:26:51 +0000

    Update 5 Pipe.c

--- Lab2/5 Pipe.c
+++ Lab2/5 Pipe.c
@@ -4,16 +4,8 @@
 #define MSGSIZE 40
 
 int main(int argc, char *argv[]) {
-    char inbuf[MSGSIZE];
+    char readmessage[MSGSIZE];
     int fd[2];
-
-    int id = fork();
-
-    // Create a child process
-    if(id == -1) {
-        printf("Could not fork process\n");
-        return 1;
-    }
 
     // Create a pipe
     if (pipe(fd) == -1) {
@@ -21,29 +13,31 @@ int main(int argc, char *argv[]) {
         return 2;
     }
 
-    if(id > 0) {
-        printf("Parent process\n");
+    // Create a child process
+    int id = fork();
+    if(id == -1) {
+        printf("Could not fork process\n");
+        return 1;
+    }
 
+    if(id > 0) {
         char* msg1 = "This is a message";
         char* msg2 = "This is another message";
 
         // Write two messages to the pipe in the parent process
-        printf("Write message 1\n");
+        printf("Write message 1: %s\n", msg1);
         write(fd[1], msg1, MSGSIZE);
-        printf("Write message 2\n");
+        
+        printf("Write message 2: %s\n", msg2);
         write(fd[1], msg2, MSGSIZE);
     } else {
-        printf("Child process\n");
-
         int nbytes;
+        
         // Read the messages from the pipe in the child process
-        while((nbytes = read(fd[0], inbuf, MSGSIZE)) > 0) {
-            printf("%s\n", inbuf);
+        while((nbytes = read(fd[0], readmessage, MSGSIZE)) > 0) {
+            printf("Read message: %s\n", readmessage);
         }
-
-        printf("Finished reading\n");
     }
 
     return 0;
 } 
- 
